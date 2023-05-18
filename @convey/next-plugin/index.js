3c5b1424c55c357929062382f97f367fb74042c1
lttb <kenzhaev.artur@gmail.com>
2023-05-19 00:50:16 +0100

    feat(next-plugin): initial implementation

---
+++ @convey/next-plugin/index.js
@@ -0,0 +1,31 @@
+/** @param {{
+ config(value: import("next/dist/server/config-shared").WebpackConfigContext): import("@convey/babel-plugin").PluginOptions
+}} pluginOptions */
+module.exports = function (pluginOptions) {
+    /** @param {import('next').NextConfig} nextConfig */
+    return (nextConfig) => {
+        /** @type {import('next').NextConfig} */
+        const config = {
+            webpack(config, options) {
+                const conveyOptions = pluginOptions.config(options);
+
+                config.module.rules.push({
+                    test: /\.(tsx|ts|js|mjs|jsx|mdx|md)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: require.resolve('./loader.cjs'),
+                        options: conveyOptions,
+                    },
+                });
+
+                if (typeof nextConfig.webpack === 'function') {
+                    return nextConfig.webpack(config, options);
+                }
+
+                return config;
+            },
+        };
+
+        return Object.assign({}, nextConfig, config);
+    };
+};
