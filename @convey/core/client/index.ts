f3f13adb25cbf0790a59ec973075b19c5b457832
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-01-26 14:02:07 +0000

    feat(core): refactor

--- @convey/core/client/index.ts
+++ @convey/core/client/index.ts
@@ -1,151 +1,73 @@
-import unfetch from 'isomorphic-unfetch';
+import { stream } from 'fetch-event-stream'
 
-import {fetchEventSource} from '@microsoft/fetch-event-source';
+import { entityReviver } from '../utils'
+import type { CancellableAsyncGenerator, ResolverOptions } from '../types'
 
-import {callbackToIter, entityReviver} from '../utils';
-import type {CancellableAsyncGenerator, ResolverOptions} from '../types';
-
-const EventStreamContentType = 'text/event-stream';
-const JSONContentType = 'application/json';
+const EventStreamContentType = 'text/event-stream'
+const JSONContentType = 'application/json'
 
 type MessageData = {
-    options: ResolverOptions;
-    payload: any;
-    error?: boolean;
-};
-
-type Message = {
-    stream?: boolean;
-    data?: MessageData;
-};
-class ExpectedError extends Error {
-    data?: MessageData;
-    constructor(data?) {
-        super('');
-
-        this.data = data;
-    }
+	options: ResolverOptions
+	// biome-ignore lint/suspicious/noExplicitAny:
+	payload: any
+	error?: boolean
 }
 
+type Message = {
+	stream?: boolean
+	data?: MessageData
+}
 type FetchedResolver = {
-    params: any;
-    options: ResolverOptions;
-    stream: boolean;
-};
+	// biome-ignore lint/suspicious/noExplicitAny:
+	params: any
+	options: ResolverOptions
+	stream: boolean
+}
 
-type RequestHeaders = Record<string, string>;
-
-const GET_LIMIT = 2048;
+type RequestHeaders = Record<string, string>
 
 export function createResolverFetcher({
-    url,
-    openWhenHidden,
-    reviver = entityReviver,
-    headers = {},
+	url,
+	reviver = entityReviver,
+	headers = {},
 }: {
-    url?: string | ((structure: FetchedResolver) => string);
-    openWhenHidden?: boolean;
-    reviver?: typeof entityReviver;
-    headers?: RequestHeaders | ((structure: FetchedResolver) => RequestHeaders);
+	url?: string | ((structure: FetchedResolver) => string)
+	openWhenHidden?: boolean
+	reviver?: typeof entityReviver
+	headers?: RequestHeaders | ((structure: FetchedResolver) => RequestHeaders)
 } = {}) {
-    return async function* fetchResolver(
-        structure: FetchedResolver
-    ): CancellableAsyncGenerator<Message> {
-        yield* callbackToIter<Message>(({done, reject, next}) => {
-            const ctrl =
-                typeof AbortController !== 'undefined'
-                    ? new AbortController()
-                    : null;
+	return async function* fetchResolver(
+		structure: FetchedResolver,
+	): CancellableAsyncGenerator<Message> {
+		const abort =
+			typeof AbortController !== 'undefined' ? new AbortController() : null
 
-            const {
-                params,
-                options: {id},
-            } = structure;
+		const {
+			params,
+			options: { id },
+		} = structure
 
-            let link =
-                typeof url === 'function'
-                    ? url(structure)
-                    : url || `/api/resolver/${id}`;
+		const link =
+			typeof url === 'function' ? url(structure) : url || `/api/resolver/${id}`
 
-            const body = JSON.stringify({params, id});
+		const body = JSON.stringify({ params, id })
 
-            let methodOptions;
+		const events = await stream(link, {
+			method: 'POST',
+			headers: {
+				accept: [JSONContentType, EventStreamContentType].join(', '),
+				'content-type': JSONContentType,
 
-            if (body.length + link.length + 1 < GET_LIMIT) {
-                methodOptions = {method: 'GET'};
-                const getParams = new URLSearchParams({b: body}).toString();
-                link += (link.includes('?') ? `&` : `?`) + getParams;
-            } else {
-                methodOptions = {
-                    method: 'POST',
-                    body: JSON.stringify({b: body}),
-                };
-            }
+				...(typeof headers === 'function' ? headers(structure) : headers),
+			},
+			body,
+			signal: abort?.signal,
+		})
 
-            fetchEventSource(link, {
-                ...methodOptions,
+		for await (const event of events) {
+			if (!event.data) continue
 
-                fetch: typeof fetch === 'undefined' ? unfetch : fetch,
-
-                signal: ctrl?.signal ?? undefined,
-
-                openWhenHidden,
-
-                headers: {
-                    accept: [JSONContentType, EventStreamContentType].join(
-                        ', '
-                    ),
-                    'content-type': JSONContentType,
-
-                    ...(typeof headers === 'function'
-                        ? headers(structure)
-                        : headers),
-                },
-
-                async onopen(response) {
-                    const contentType = response.headers.get('content-type');
-
-                    if (contentType?.startsWith(JSONContentType)) {
-                        throw new ExpectedError(
-                            JSON.parse(await response.text(), reviver)
-                        );
-                    }
-
-                    if (!contentType?.startsWith(EventStreamContentType)) {
-                        throw new Error(
-                            `Expected content-type to be ${EventStreamContentType}, Actual: ${contentType}`
-                        );
-                    }
-                },
-
-                onerror(err) {
-                    if (err instanceof ExpectedError) {
-                        done({stream: false, data: err.data});
-
-                        throw new ExpectedError();
-                    } else {
-                        reject(err);
-                    }
-                },
-
-                onmessage(message) {
-                    next({data: JSON.parse(message.data, reviver)});
-                },
-
-                onclose() {
-                    done({stream: true});
-                },
-            }).catch((err) => {
-                if (err instanceof ExpectedError) {
-                    return;
-                }
-
-                reject(err);
-            });
-
-            return () => {
-                if (ctrl) ctrl.abort();
-            };
-        });
-    };
+			yield { data: JSON.parse(event.data, reviver) }
+		}
+	}
 }
